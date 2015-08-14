d64b519762536fe0f99b4059f9e88a3a45e30151
khovratovich <khovratovich@gmail.com>
2015-08-14 14:25:48 +0200

    Code refactored
    More comments, better structure

--- Argon2d/ref/argon2d-ref.cpp
+++ Argon2d/ref/argon2d-ref.cpp
@@ -25,6 +25,64 @@ using namespace std;
 
 #define BLOCK(lane,slice,index) ((index)+(lane)*segment_length+(slice)*segment_length*lanes)
 
+/*
+KAT function that prints the inputs to the file
+*/
+void InitialKat(uint8_t* blockhash, uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
+	uint8_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
+{
+	FILE* fp = fopen(KAT_FILENAME, "a+");
+
+	fprintf(fp, "=======================================\n");
+	fprintf(fp, "Iterations: %d, Memory: %d KBytes, Parallelism: %d lanes, Tag length: %d bytes\n", t_cost, m_cost, lanes, outlen);
+
+
+
+	fprintf(fp, "Message: ");
+	for (unsigned i = 0; i<msglen; ++i)
+		fprintf(fp, "%2.2x ", ((unsigned char*)msg)[i]);
+	fprintf(fp, "\n");
+	fprintf(fp, "Nonce: ");
+	for (unsigned i = 0; i<noncelen; ++i)
+		fprintf(fp, "%2.2x ", ((unsigned char*)nonce)[i]);
+	fprintf(fp, "\n");
+	fprintf(fp, "Input Hash: ");
+	for (unsigned i = 0; i<INPUT_INITIAL_HASH_LENGTH; ++i)
+		fprintf(fp, "%2.2x ", ((unsigned char*)blockhash)[i]);
+	fprintf(fp, "\n");
+	fclose(fp);
+}
+
+/*
+Function that prints the internal state
+*/
+void InternalKat(block* state, uint32_t m_cost, uint32_t pass)
+{
+	FILE* fp = fopen(KAT_FILENAME, "a+");
+	fprintf(fp, "\n After pass %d:\n", pass);
+	for (uint32_t i = 0; i < m_cost; ++i)
+	{
+		fprintf(fp, "Block %.4d [0]: %x\n", i, state[i][0]);
+
+	}
+	fclose(fp);
+}
+
+/*
+Function that prints the output tag
+*/
+void PrintTag(void* out, uint32_t outlen)
+{
+	FILE* fp = fopen(KAT_FILENAME, "a+");
+	fprintf(fp, "Tag: ");
+	for (unsigned i = 0; i<outlen; ++i)
+		fprintf(fp, "%2.2x ", ((uint8_t*)out)[i]);
+	fprintf(fp, "\n");
+	fclose(fp);
+}
+
+	
+
 void allocate_memory(block **memory, uint32_t m_cost)
 {
 	*memory = new block[m_cost];
@@ -43,8 +101,11 @@ void free_memory(block *memory)
 	}
 }
 
+/*
+*Function creates a new memory block*
+**/
 
-void MakeBlock(block* prev_block, block* ref_block, block* next_block)
+void MakeBlock(block* prev_block, block* ref_block, block* next_block) 
 {
 	block blockR = *prev_block ^ *ref_block;
 	block blocktmp = blockR;
@@ -69,9 +130,9 @@ void MakeBlock(block* prev_block, block* ref_block, block* next_block)
 }
 
 
-
-
-
+/****
+*Function that fills the segment using previous segments also from other threads
+****/
 void FillSegment(block* state, uint32_t m_cost, uint8_t lanes, uint32_t round, uint8_t lane, uint8_t slice)//Filling "slice" in "lane" and "round"
 {
 	uint32_t segment_length = m_cost /(lanes* (uint32_t)SYNC_POINTS);   //Computing length of the slice
@@ -81,44 +142,38 @@ void FillSegment(block* state, uint32_t m_cost, uint8_t lanes, uint32_t round, u
 	uint32_t pseudo_rand, ref_index, ref_lane, ref_slice;
 	block addresses;
 	block prev_block;  //previous block
-	for (uint32_t i = 0; i < segment_length; ++i)
+	for (uint32_t i = ((round == 0) && (slice == 0))?2:0; i < segment_length; ++i)
 	{
-		/*0.Computing addresses if necessary*/
-		if ((round == 0) && (slice == 0) && (i < 2)) //skip first two blocks
-			continue;
-
-		/*Reading previous block*/
+		
+		/*1.1. Computing the index of the previous block*/
 		uint32_t prev_index;
-		uint32_t recalculation_start=0;
 		if ((round == 0) && (slice == 0) && (i == 2))
 			prev_index = BLOCK(lane, 0, 1);
-		if (i == 0)//not round 0, slice 0
+		else if (i == 0)//not round 0 & slice 0
 		{
 			if (slice == 0)
-			{
-				recalculation_start = BLOCK(0, SYNC_POINTS - 2, 0);
 				prev_index = BLOCK(lane, SYNC_POINTS - 1, segment_length - 1);
-			}
 			else
-			{
-				recalculation_start = BLOCK(0, slice - 1, 0);
 				prev_index = BLOCK(lane, slice - 1, segment_length - 1);
-			}
 		}
 		else prev_index = BLOCK(lane, slice, i - 1);
 		prev_block = state[prev_index];
 
-		/*1. Computing the reference block*/
-		/*1.1 Taking pseudo-random value from the previous block */
+		/*1.2. Computing the index of the reference block*/
+		/* 1.2.1 Taking pseudo-random value from the previous block */
 		pseudo_rand = *(uint32_t*)prev_block.v;
-
-		/*1.2 Computing reference block location*/
-		/*NEW*/
+		/*1.2.2 Computing the lane of the reference block*/
 		ref_lane = (pseudo_rand >> 24) % lanes;
 
+		/*1.2.3 Computing the number of possible reference block within the lane. 
+		    Pass0:
+			  This lane: all already finished segments plus already constructed blocks in this segment
+			  Other lanes: all already finished segments
+			Pass1+:
+			  This lane:  (SYNC_POINTS-1) last segments plus already constructed blocks in this segment
+			  Other lanes:  (SYNC_POINTS-1) last segments*/
 		if (round == 0)//first pass
 		{
-			start_position = 0;
 			if (slice == 0)//first slice
 			{
 				ref_lane = lane;
@@ -130,42 +185,45 @@ void FillSegment(block* state, uint32_t m_cost, uint8_t lanes, uint32_t round, u
 		}
 		else //second pass, no first slice rule
 		{
-			start_position = (slice == SYNC_POINTS - 1) ? 0 : (slice + 1)*segment_length;
 			if (ref_lane==lane)//the same lane
 				reference_area_size = (SYNC_POINTS-1)*segment_length + i - 1;
 			else reference_area_size = (SYNC_POINTS - 1)*segment_length + ((i == 0) ? (-1) : 0);
 		}
 
-		uint64_t position = (pseudo_rand & 0xFFFFFF); //last 24 bits for the position
-		position = position*position >> 24;
-		position = reference_area_size - 1 - ((reference_area_size - 1)*position >> 24);//mapping pseudo-rand into [0;reference_area_size)
+		/*1.2.4. Mapping pseudo_rand to 0..<reference_area_size-2> and produce relative position*/
+		uint64_t relative_position = (pseudo_rand & 0xFFFFFF); //last 24 bits for the position
+		relative_position = relative_position*relative_position >> 24;
+		relative_position = reference_area_size - 1 - ((reference_area_size - 1)*relative_position >> 24);
 
-		position = (start_position + position) % (SYNC_POINTS*segment_length); //absolute position
+		/*1.2.5 Computing starting position*/
+		if (round == 0)
+			start_position = 0;
+		else start_position = (slice == SYNC_POINTS - 1) ? 0 : (slice + 1)*segment_length;
+
+		/*1.2.6. Computing absolute position*/
+		uint64_t position = (start_position + relative_position) % (SYNC_POINTS*segment_length); //absolute position
 		ref_index = position %segment_length;
 		ref_slice = position / segment_length;
 
-		/*ENDNEW*/
-		
 		/*2.Creating a new block*/
-		block ref_block = state[BLOCK(ref_lane,ref_slice,ref_index)];  //random block from memory
-		
+		block ref_block = state[BLOCK(ref_lane,ref_slice,ref_index)];  
 		block* next_block = &(state[BLOCK(lane, slice, i)]);
-		//printf("Ref: %.2d Next:%.2d\n", (ref_block - state) / BYTES_IN_BLOCK, (next_block - state) / BYTES_IN_BLOCK);
 		MakeBlock(&prev_block, &ref_block, next_block);  //Create new block
-		
 	}
-
 }
 
-block*  Initialize(uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
-	uint8_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
-{
 
-	//Initial hashing
-	uint8_t blockhash[BLAKE_INPUT_HASH_SIZE + 8];//H_0 in the document
+
+/*
+* Hashes all the inputs into blockhash[INPUT_INITIAL_HASH_LENGTH]
+* TODO: make it endianness-independent
+*/
+void InitialHash(uint8_t* blockhash, uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
+	uint32_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
+{
 	uint8_t version = VERSION_NUMBER;
 	blake2b_state BlakeHash;
-	blake2b_init(&BlakeHash, BLAKE_INPUT_HASH_SIZE);
+	blake2b_init(&BlakeHash, INPUT_INITIAL_HASH_LENGTH);
 
 	blake2b_update(&BlakeHash, (const uint8_t*)&lanes, sizeof(lanes));
 	blake2b_update(&BlakeHash, (const uint8_t*)&outlen, sizeof(outlen));
@@ -182,78 +240,81 @@ block*  Initialize(uint32_t outlen, const uint8_t *msg, uint32_t msglen, const u
 	blake2b_update(&BlakeHash, (const uint8_t*)ad, adlen);
 
 
-	blake2b_final(&BlakeHash, blockhash, BLAKE_INPUT_HASH_SIZE);
-	memset(blockhash + BLAKE_INPUT_HASH_SIZE, 0, 8);
+	blake2b_final(&BlakeHash, blockhash, INPUT_INITIAL_HASH_LENGTH);
+}
 
-#ifdef KAT
-	FILE* fp = fopen(KAT_FILENAME, "a+");
+/*
+* Function creates first 2 blocks per lane
+ */
+void MakeFirstBlocks(uint8_t* blockhash, block* state, uint32_t m_cost, uint8_t lanes)
+{
+	uint32_t segment_length = m_cost / (lanes* (uint32_t)SYNC_POINTS);
+	for (uint8_t l = 0; l < lanes; ++l)  //Make the first and second block in each lane as G(H0||i||0) or G(H0||i||1)
+	{
+		blockhash[INPUT_INITIAL_HASH_LENGTH + 4] = l;
+		blockhash[INPUT_INITIAL_HASH_LENGTH] = 0;
+		blake2b_long((uint8_t*)&(state[l*segment_length]), blockhash, BLOCK_SIZE, INPUT_INITIAL_HASH_LENGTH + 8);
+		blockhash[INPUT_INITIAL_HASH_LENGTH] = 1;
+		blake2b_long((uint8_t*)&(state[l*segment_length + 1]), blockhash, BLOCK_SIZE, INPUT_INITIAL_HASH_LENGTH + 8);
+	}
+}
 
-	fprintf(fp, "=======================================\n");
-	fprintf(fp, "Iterations: %d, Memory: %d KBytes, Parallelism: %d lanes, Tag length: %d bytes\n", t_cost, m_cost, lanes, outlen);
 
-
-
-	fprintf(fp, "Message: ");
-	for (unsigned i = 0; i<msglen; ++i)
-		fprintf(fp, "%2.2x ", ((unsigned char*)msg)[i]);
-	fprintf(fp, "\n");
-	fprintf(fp, "Nonce: ");
-	for (unsigned i = 0; i<noncelen; ++i)
-		fprintf(fp, "%2.2x ", ((unsigned char*)nonce)[i]);
-	fprintf(fp, "\n");
-	fprintf(fp, "Input Hash: ");
-	for (unsigned i = 0; i<BLAKE_INPUT_HASH_SIZE; ++i)
-		fprintf(fp, "%2.2x ", ((unsigned char*)blockhash)[i]);
-	fprintf(fp, "\n");
-	fclose(fp);
-#endif
-
-	//Memory allocation
+/*
+* Function hashes the inputs with Blake, allocates memory, and creates first two blocks. Returns the pointer to the main memory with 2 blocks per lane
+* initialized
+*/
+block*  Initialize(uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
+	uint32_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
+{
+	//1. Memory allocation
 	block* state;
 	allocate_memory(&state, m_cost);
 
-	//Creating first blocks, we always have at least two blocks in a slice
 
-	uint32_t segment_length = m_cost / (lanes* (uint32_t)SYNC_POINTS);
-	for (uint8_t l = 0; l < lanes; ++l)
-	{
-		blockhash[BLAKE_INPUT_HASH_SIZE + 4] = l;
-		blockhash[BLAKE_INPUT_HASH_SIZE] = 0;
-		blake2b_long((uint8_t*)&(state[l*segment_length]), blockhash, BLOCK_SIZE, BLAKE_INPUT_HASH_SIZE + 8);
-		blockhash[BLAKE_INPUT_HASH_SIZE] = 1;
-		blake2b_long((uint8_t*)&(state[l*segment_length + 1]), blockhash, BLOCK_SIZE, BLAKE_INPUT_HASH_SIZE + 8);
-	}
-	memset(blockhash, 0, BLAKE_INPUT_HASH_SIZE + 8);
+	//2. Initial hashing  TODO - put in a separate function
+	uint8_t blockhash[INPUT_INITIAL_HASH_LENGTH + 8];//H_0 +8 extra bytes to produce the first blocks
+	InitialHash(blockhash, outlen, msg, msglen, nonce, noncelen, secret, secretlen, ad, adlen, t_cost, m_cost, lanes); //Hashing all inputs
+	memset(blockhash + INPUT_INITIAL_HASH_LENGTH, 0, 8); //zeroing 8 extra bytes
+
+#ifdef KAT
+	InitialKat(blockhash, outlen, msg, msglen, nonce, noncelen, secret, secretlen, ad, adlen, t_cost, m_cost, lanes);
+#endif
+
+	//3. Creating first blocks, we always have at least two blocks in a slice
+	MakeFirstBlocks(blockhash, state, m_cost, lanes);
+	memset(blockhash, 0, INPUT_INITIAL_HASH_LENGTH + 8); //clearing the hash
 	return state;
 }
 
-void Finalize(block* state, uint8_t *out, uint32_t outlen, uint32_t m_cost, uint8_t lanes)//XORing the last block of each lane, hashing it, making the tag.
+/*
+* 
+*XORing the last block of each lane, hashing it, making the tag.
+*/
+void Finalize(block* state, uint8_t *out, uint32_t outlen, uint32_t m_cost, uint8_t lanes)
 {
 	block blockhash;
 	uint32_t segment_length = m_cost / (lanes* (uint32_t)SYNC_POINTS);
 
+	//XOR the last blocks
 	for (uint8_t l = 0; l < lanes; ++l)
 	{
 		blockhash = blockhash^ state[BLOCK(l, SYNC_POINTS - 1, segment_length - 1)];
 	}
 
-	blake2b_long(out, blockhash.v, outlen, BLOCK_SIZE);
-
-	free_memory(state);
-
+	blake2b_long(out, blockhash.v, outlen, BLOCK_SIZE);//Hash the XOR
+	free_memory(state); //Deallocate the memory
 #ifdef KAT
-	FILE* fp = fopen(KAT_FILENAME, "a+");
-	fprintf(fp, "Tag: ");
-	for (unsigned i = 0; i<outlen; ++i)
-		fprintf(fp, "%2.2x ", ((uint8_t*)out)[i]);
-	fprintf(fp, "\n");
-	fclose(fp);
+	PrintTag(out,outlen);
 #endif 
 	memset(blockhash.v, 0, 64 * 16);
-
-	
 }
 
+
+
+/*
+Function that fills the entire memory t_cost times based on the first two blocks in each lane
+*/
 void FillMemory(block* state, uint32_t t_cost, uint32_t m_cost, uint8_t lanes) //Main loop: filling memory <t_cost> times
 {
 	for (uint8_t r = 0; r < t_cost; ++r)
@@ -266,67 +327,75 @@ void FillMemory(block* state, uint32_t t_cost, uint32_t m_cost, uint8_t lanes) /
 			}
 		}
 #ifdef KAT_INTERNAL
-		FILE* fp = fopen(KAT_FILENAME, "a+");
-		fprintf(fp, "\n After pass %d:\n", r);
-		for (uint32_t i = 0; i < m_cost; ++i)
-		{
-			fprintf(fp, "Block %.4d [0]: %x\n", i, state[i][0]);
-
-		}
-		fclose(fp);
+		InternalKat(state,m_cost,r);
 #endif
 	}
 }
 
-/*reference version*/
-int Argon2d(uint8_t *out, uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
-	uint8_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
+/*
+Function that validates all inputs against predefined restrictions and return an error code
+*/
+int ValidateInputs(uint8_t *out, uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
+	uint32_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
 {
-	if (outlen>MAX_OUTLEN)
-		outlen = MAX_OUTLEN;
+	if (out == NULL)
+		return NULL_OUTPUT_PTR;
+
+	if (outlen > MAX_OUTLEN)
+		return TOO_LONG_OUTPUT;
 	if (outlen < MIN_OUTLEN)
-		return -1;  //Tag too short
+		return TOO_SHORT_OUTPUT;  //Tag too short
 
-	if (msglen> MAX_MSG)
-		msglen = MAX_MSG;
-	if (msglen < MIN_MSG)
-		return -2; //Password too short
+	if (msglen > MAX_PWD_LENGTH)
+		return TOO_LONG_PWD;
+	if (msglen < MIN_PWD_LENGTH)
+		return TOO_SHORT_PWD; //Password too short
 
-	if (noncelen < MIN_NONCE)
-		return -3; //Salt too short
-	if (noncelen> MAX_NONCE)
-		noncelen = MAX_NONCE;
+	if (noncelen < MIN_SALT_LENGTH)
+		return TOO_SHORT_SALT; //Salt too short
+	if (noncelen > MAX_SALT_LENGTH)
+		return TOO_LONG_SALT;
 
-	if (secretlen> MAX_SECRET)
-		secretlen = MAX_SECRET;
+	if (secretlen > MAX_SECRET)
+		return TOO_LONG_SECRET;
 	if (secretlen < MIN_SECRET)
-		return -4; //Secret too short
+		return TOO_SHORT_SECRET; //Secret too short
 
-	if (adlen> MAX_AD)
-		adlen = MAX_AD;
-	if (adlen < MIN_AD)
-		return -5; //Associated data too short
+	if (adlen > MAX_AD_LENGTH)
+		return TOO_LONG_AD;
+	if (adlen < MIN_AD_LENGTH)
+		return TOO_SHORT_AD; //Associated data too short
 
 	//minumum m_cost =8L blocks, where L is the number of lanes
 	if (m_cost < 2 * SYNC_POINTS*lanes)
-		m_cost=2 * SYNC_POINTS*lanes;
-	if (m_cost>MAX_MEMORY)
-		m_cost = MAX_MEMORY;
+		m_cost = 2 * SYNC_POINTS*lanes;
+
+	if (m_cost > MAX_MEMORY)
+		return TOO_MUCH_MEMORY;
 
 	m_cost = (m_cost / (lanes*SYNC_POINTS))*(lanes*SYNC_POINTS); //Ensure that all segments have equal length;
 
-	//minimum t_cost =3
-	if (t_cost<MIN_TIME)
-		t_cost = MIN_TIME;
+	if (t_cost < MIN_TIME)
+		return TOO_SMALL_TIME;
+	if (t_cost > MIN_TIME)
+		return TOO_LARGE_TIME;
 
 	if (lanes<MIN_LANES)
-		lanes = MIN_LANES;
-	if (lanes>m_cost / BLOCK_SIZE_KILOBYTE)
-		lanes = m_cost / BLOCK_SIZE_KILOBYTE;
+		return TOO_FEW_LANES;
+	if (lanes > MAX_LANES)
+		return TOO_MANY_LANES;
+	return 0;
+}
 
-	//printf("Argon2d called, %d m_cost %d lanes\n", m_cost, lanes);
-
-
+/*Function that performs memory-hard hashing with certain degree of parallelism*/
+int Argon2d(uint8_t *out, uint32_t outlen, const uint8_t *msg, uint32_t msglen, const uint8_t *nonce, uint32_t noncelen, const uint8_t *secret,
+	uint32_t secretlen, const uint8_t *ad, uint32_t adlen, uint32_t t_cost, uint32_t m_cost, uint8_t lanes)
+{
+	/*0. Validate all inputs*/
+	int check_value = ValidateInputs(out, outlen, msg, msglen, nonce, noncelen, secret, secretlen, ad, adlen, t_cost, m_cost, lanes);
+	if (check_value != 0)
+		return check_value;
+	
 	/*1. Initialization: Hashing inputs, allocating memory, filling first blocks*/
 	block* state = Initialize( outlen, msg, msglen, nonce, noncelen, secret, secretlen, ad, adlen, t_cost, m_cost, lanes);//
 
